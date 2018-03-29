6541b6512454a90c72120d77247e9a6e8cb20e12
naglaasayed <36450220+naglaasayed@users.noreply.github.com>
2018-03-29 16:43:10 +0200

    Add files via upload

---
+++ ميرنا2.cpp
@@ -0,0 +1,87 @@
+#include<iostream>
+#include<string>
+using namespace std;
+int main(){
+string word;
+cout<<"enter word  ";
+cin>>word;
+for(i=0 ;i<=word.size(); i++){
+    if word[i]=="a"{
+        cout,,"11";}
+     else if word[i]=="b"{
+        cout<<"12";
+     }
+     else if word[i]=="c"{
+        cout<<"13";
+     }
+     else if word[i]=="d"{
+        cout<<"44";
+     }
+     else if word[i]=="e"{
+        cout<<"15";
+     }
+      else if word[i]=="f"{
+        cout<<"21";
+
+      }
+      else if word[i]=="g"{
+        cout<<"22";
+      }
+      else if word[i]=="h"{
+        cout<<"23";
+      }
+      else if word[i]=="i"{
+        cout<<"24";
+      }
+      else if word[i]=="j"{
+        cout<<"25";
+      }
+      else if word[i]=="k"{
+        cout<<"31";
+      }
+      else if word[i]=="l"{
+        cout<<"32";
+      }
+      else if word[i]=="m"{
+        cout<<"33";
+      }
+      else if word[i]=="n"{
+        cout<<"34";
+      }
+      else if word[i]=="o"{
+        cout<<"35";
+      }
+      else if word[i]=="p"{
+        cout<<"41";
+      }
+      else if word[i]=="q"{
+        cout<<"42";
+      }
+      else if word[i]=="r"{
+        cout<<"43";
+      }
+      else if word[i]=="s"{
+        cout<<"44"
+      }
+      else if word[i]=="t"{
+        cout<<"45";
+      }
+      else if word[i]=="u"{
+        cout<<"45";
+      }
+      else if word[i]=="v"{
+        cout<<"52";
+      }
+      else if word[i]=="x"{
+        cout<<"53";
+      }
+      else if word[i]=="y"{
+        cout<<"54";
+      }
+      else if word[i]=="z"{
+        cout<<"55";
+      }
+return 0;
+}
+
+}
